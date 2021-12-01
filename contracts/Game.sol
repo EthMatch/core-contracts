1be618c9fb76d1799086810036c885fb3de5cf4b
pradeep-selva <pradeepselvaraju@gmail.com>
2021-12-01 17:35:55 +0530

    ft: Game.sol :: IERC20 player functions

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Game {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
     IERC20 public wantToken;
     bool public isEthGame;
 
@@ -19,5 +24,36 @@ contract Game {
         } else {
             isEthGame = true;
         }
+    }
+
+    function createNewAccount(uint256 _initialDeposit) external onlyIERC20 {
+        require(!accounts[msg.sender], "Player already exists!");
+
+        accounts[msg.sender] = true;
+        if (_initialDeposit > 0) {
+            _takeDeposits(msg.sender, _initialDeposit);
+        }
+    }
+
+    function depositBalances(uint256 _amountIn) external onlyIERC20 {
+        require(accounts[msg.sender], "Player does not exist!");
+        _takeDeposits(msg.sender, _amountIn);
+    }
+
+    function _takeDeposits(address _depositor, uint256 _amountIn) internal {
+        require(_amountIn > 0, "Invalid deposit amount");
+
+        wantToken.safeTransferFrom(_depositor, address(this), _amountIn);
+        accountBalances[_depositor] += _amountIn;
+    }
+
+    modifier onlyEth() {
+        require(isEthGame, "You cannot deposit IERC20 tokens");
+        _;
+    }
+
+    modifier onlyIERC20() {
+        require(!isEthGame, "You cannot deposit ETH");
+        _;
     }
 }
